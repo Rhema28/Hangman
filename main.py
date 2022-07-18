7d0c9e1eee4c36f751b713588ca5c51bca476024
Rhema Joseph <rhemajoseph2@gmail.com>
2022-07-18 20:35:01 +0000

    Draw 7 structures for each level of hangman game

---
+++ main.py
@@ -0,0 +1,67 @@
+# Hangman Game
+# Computer picks a word from a list
+# Player makes a guess of one letter 
+# at a time 
+
+
+#imports
+import random
+
+#Constants
+HANGMAN = ['''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+===========''', '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+===========''', '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+===========''', '''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+===========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+===========''', '''
+ +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+===========''', '''
+ +---+
+    |   |
+    O   |
+   /|\  |
+   / \    |
+        |
+===========''', ''']
+
+WORDS = ["HEART", "LOBSTER", "RABBIT", "MOTHERHOOD", "STAR", PINEAPPLE"]
+ 
+MAX_WRONG = len(HANGMAN) - 1
+
+
+# Initialise Variables
